a9d0a77899d283f00ede2ad3692b4e3d7b255a14
arizkyrahman <rizkyrahman7202@gmail.com>
2023-07-10 14:55:07 +0700

    survey view init

--- react/src/views/SurveyView.jsx
+++ react/src/views/SurveyView.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
 import PageComponent from "../components/PageComponent";
 
 export const SurveyView = () => {
-  return <PageComponent title="Create new Survey">Survey Form</PageComponent>;
+  const [survey, setSurvey] = useState({
+    title: "",
+    slug: "",
+    status: false,
+    description: "",
+    image: null,
+    image_url: null,
+    expire_date: "",
+    questions: [],
+  });
+
+  return (
+    <PageComponent title="Create new Survey">
+      <form action="#" method="post" onSubmit={onSubmit}>
+        <div className="shadow sm:overflow-hidden sm:rounded-md">
+          <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+            {/*image*/}
+
+            {/*image*/}
+          </div>
+        </div>
+      </form>
+    </PageComponent>
+  );
 };
