3f18a7ccf224d138264a0b5578f5e18d176e401f
arizkyrahman <rizkyrahman7202@gmail.com>
2023-07-13 14:53:09 +0700

    sign up method init

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\SignupRequest;
-use App\Http\Controllers\LoginRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 
 class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
     {
+        $data = $request->validated();
+
+        /** @var \App\Models\User $user */
+        $user = User::Create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
     }
 
     public function login(LoginRequest $request)
