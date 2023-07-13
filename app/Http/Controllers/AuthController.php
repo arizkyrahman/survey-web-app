e64804d424e72c6c60a564d3ebf5d29e8da333d0
arizkyrahman <rizkyrahman7202@gmail.com>
2023-07-13 14:43:15 +0700

    auth init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\SignupRequest;
+use App\Http\Controllers\LoginRequest;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+    }
+
+    public function login(LoginRequest $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+    }
+}
