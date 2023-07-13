47d2dce5622174a52a5b21368931ece75f7c113f
arizkyrahman <rizkyrahman7202@gmail.com>
2023-07-13 16:30:05 +0700

    login req init

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,6 +28,22 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
+        $credentials = $request->validated();
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'The provided credentials are not correct'
+            ], 422);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
     }
 
     public function logout(Request $request)
