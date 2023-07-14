a583b93615210dcbf6ddced08cf077786f1c9456
arizkyrahman <rizkyrahman7202@gmail.com>
2023-07-14 21:46:19 +0700

    sign up page init

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
     <>
@@ -9,6 +11,23 @@ export default function Signup() {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="full-name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Full Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="full-name"
+                name="name"
+                type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
           <div>
             <label
               htmlFor="email"
@@ -36,14 +55,6 @@ export default function Signup() {
               >
                 Password
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -51,6 +62,26 @@ export default function Signup() {
                 name="password"
                 type="password"
                 autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password-confirmation"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password Confirmation
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password-confirmation"
+                name="password_confirmation"
+                type="password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -68,13 +99,13 @@ export default function Signup() {
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{" "}
-          <a
-            href="#"
+          Or{" "}
+          <Link
+            to="/login"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
-            Start a 14 day free trial
-          </a>
+            Login with your account
+          </Link>
         </p>
       </div>
     </>
